52e22544662b50ceee8443ab93b91256dca244d8
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2025-09-22 22:03:47 -0500

    Link init app to index js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import "./styles.css";
 
 import "./modules/app/barrel.js";
+
+import { initializeApp } from "./modules/app/init";
+
+document.addEventListener("DOMContentLoaded", () => {
+    initializeApp();
+});
