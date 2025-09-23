e187e8b9acae5181aa86f08dc1eda41fae5b7a89
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2025-09-22 22:11:18 -0500

    Create close dialog listeners

--- src/modules/dom/eventListeners.js
+++ src/modules/dom/eventListeners.js
@@ -3,6 +3,8 @@
 const newProjectButton = document.getElementById("new-project-button");
 const projectModal = document.getElementById("new-project-modal");
 
+// Modals //
+
 export function showDialog(type) {
     if (type === "project") {
         projectModal.showModal();
@@ -12,5 +14,19 @@ export function showDialog(type) {
 export function showProjectModal() {
     newProjectButton.addEventListener("click", () => {
         showDialog("project");
+    })
+}
+
+export function closeDialog(element) {
+    element.close();
+}
+
+export function closeDialogAction() {
+    const cancelBtn = document.querySelectorAll("button.cancel-button");
+
+    cancelBtn.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            closeDialog(projectModal);
+        })
     })
 }
