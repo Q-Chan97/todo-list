574fc3900cd0e79f119cfa25d27d68d0012ad426
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2025-10-03 22:31:55 -0500

    Update

--- src/modules/app/barrel.js
+++ src/modules/app/barrel.js
@@ -1,6 +1,5 @@
 export * from "./tasks.js";
 export * from "./projects.js";
 export * from "../dom/dom.js";
-export * from "../dom/siteNav.js"
 export * from "../dom/projectRender.js";
 export * from "../dom/eventListeners.js";
